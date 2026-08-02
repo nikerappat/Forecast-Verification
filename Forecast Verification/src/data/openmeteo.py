159ce6aafa7663bc8f122f9bfe1acf9869f04702
nikerappat <nike.rappat@gmail.com>
2026-08-02 13:46:10 +0200

    Add Open-Meteo data module structure

---
+++ Forecast Verification/src/data/openmeteo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  2 13:45:13 2026
+
+@author: niker
+"""
+
+def get_forecast():
+    """
+    Placeholder for Open-Meteo forecast download.
+    """
+    print("Open-Meteo connection will be implemented here")
+
+
+if __name__ == "__main__":
+    get_forecast()
