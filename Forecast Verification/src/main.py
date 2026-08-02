757b1cb291a74dfb21efc184281c5311afc3c51a
nikerappat <nike.rappat@gmail.com>
2026-08-02 13:19:50 +0200

    Add initial Python entry point

---
+++ Forecast Verification/src/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  2 13:13:50 2026
+
+@author: niker
+"""
+
+def main():
+    print("Forecast Verification Tool")
+
+
+if __name__ == "__main__":
+    main()
